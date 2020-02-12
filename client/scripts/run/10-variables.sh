0b0793d2c9d718713802b09a19b603f0def8c842
Timothee Boussus <timothee.boussus@gmail.com>
2020-02-12 17:45:54 +0100

    Replaced sed delimiter to work with slashes

--- client/scripts/run/10-variables.sh
+++ client/scripts/run/10-variables.sh
@@ -6,7 +6,7 @@ cp /app/public/variables.js $VAR_PATH
 
 if [ ! -z "$API_ENDPOINT" ]
 then
-    sed -i "s/__API_ENDPOINT__/$API_ENDPOINT/g" "$VAR_PATH"
+    sed -i "s;__API_ENDPOINT__;$API_ENDPOINT;g" "$VAR_PATH"
 else
     echo "API_ENDPOINT is not defined, web app won't work"
     exit 1
