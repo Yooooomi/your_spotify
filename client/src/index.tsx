f64842d4f567d59f28884bab0d77cdf06555191a
Timothée Boussus <timothee.boussus@gmail.com>
2022-05-08 14:08:53 +0200

    -BlankPage: fixed blank page when not admin

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './services/redux';
 
-ReactDOM.render(
+const element = document.getElementById('root');
+const root = createRoot(element!);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
