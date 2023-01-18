81feab56b84e18e1b5ca95e5b53da9562082673c
Timothee <timothee.boussus@gmail.com>
2023-01-19 00:18:20 +0100

    Fixed index html

--- client/scripts/run/variables.sh
+++ client/scripts/run/variables.sh
@@ -9,14 +9,15 @@ else
     VAR_PATH="/app/build"
 fi
 
-cp "$VAR_PATH/index-template.html" "$VAR_PATH/index.html"
 cp "$VAR_PATH/variables-template.js" "$VAR_PATH/variables.js"
 
 if [ ! -z "$API_ENDPOINT" ]
 then
     echo "Setting API Endpoint to '$API_ENDPOINT'"
     sed -i "s;__API_ENDPOINT__;$API_ENDPOINT;g" "$VAR_PATH/variables.js"
-    sed -i "s;__API_ENDPOINT__;$API_ENDPOINT;g" "$VAR_PATH/index.html"
+
+    # Editing meta image urls
+    sed -i "s;image\" content=\"\(.[^\"]*\);image\" content=\"$API_ENDPOINT/static/your_spotify_1200.png;g" "$VAR_PATH/index.html"
 else
     echo "API_ENDPOINT is not defined, web app won't work"
     exit 1
