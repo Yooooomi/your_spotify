3aec35321897ddb40a93acf494f5f2e1e2528bc8
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-08-18 15:44:50 +0200

    example docker yml uses production build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd client && docker build -f Dockerfile.production . -t yooooomi/your_spotify_client:latest ; cd -
-cd server && docker build -f Dockerfile.production . -t yooooomi/your_spotify_server:latest ; cd -
+cd client && docker build -f Dockerfile.production . -t fabalexsie/your_spotify_client:latest ; cd -
+cd server && docker build -f Dockerfile.production . -t fabalexsie/your_spotify_server:latest ; cd -
