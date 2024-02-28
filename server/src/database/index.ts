4182f7d91cc2640670639f9240c7c82b41489a87
Romain NEUPLANCHE <rneuplanche@smartadserver.com>
2024-02-28 20:30:31 +0100

    Fix

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -8,7 +8,6 @@ export * from './queries/global';
 export * from './queries/artist';
 export * from './queries/track';
 export * from './queries/album';
-export * from './queries/playlist';
 
 export const connect = async () => {
   const fallbackConnection = 'mongodb://mongo:27017/your_spotify';
