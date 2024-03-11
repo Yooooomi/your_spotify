612075f71ebfb8552b7482bc8a9da8fc01971a21
bloedboemmel <jakob.jung@fau.de>
2024-03-11 22:04:07 +0100

    Reverted docker-changes

--- apps/server/scripts/run/run_dev.sh
+++ apps/server/scripts/run/run_dev.sh
@@ -2,4 +2,4 @@
 
 source /app/apps/server/scripts/run/deprecated.sh
 
-cd apps/server && yarn build && yarn migrate && yarn dev -o --legacy-watch --watch "src/**" --ext "ts,json" --ignore "src/**/*.spec.ts" --exec "ts-node src/index.ts"
+cd apps/server && yarn build && yarn migrate && yarn dev
