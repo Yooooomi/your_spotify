66c0b101b47e693324fdd8a5a208954537fb2256
Webysther Sperandio <webysther@gmail.com>
2025-03-21 04:51:49 +0100

    Using client

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,4 +1,4 @@
-import { connect as connectToDb, Mongoose, mongoose } from "mongoose";
+import { connect as connectToDb, Mongoose } from "mongoose";
 import { getWithDefault } from "../tools/env";
 import { logger } from "../tools/logger";
 import { wait } from "../tools/misc";
@@ -32,7 +32,7 @@ export const connect = async () => {
   if (!client) {
     throw lastError;
   }
-  mongoose.set("allowDiskUse", true);
+  client.set("allowDiskUse", true);
   logger.info("Connected to database !");
   return client;
 };
